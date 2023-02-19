53a549cbb63db50ae50e88b82bae17fe8751f5e8
bashadmin <dev.sugarman>
2023-02-18 23:50:01 -0500

    initial commit

---
+++ random_tests/hw.java
@@ -0,0 +1,9 @@
+// #!/usr/bin/java
+class Message {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello User.");
+		String endMessage = "Goodbye User";
+		System.out.println(endMessage);
+	}
+}
